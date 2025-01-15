eb68d72faea895e81a8ab552a856b72b4cf4cab4
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-15 12:03:39 -0300

    Update switches.php

--- switches.php
+++ switches.php
@@ -3,7 +3,53 @@
   //switch = replacement to using many elseif statements
   //         more efficient, less code to write
 
+  // $grade = "A";
+
+  // if($grade == "A"){
+  //   echo "You did great";
+  // }
+  // else if($grade == "B"){
+  //   echo "You did good";
+  // }
+  // else if($grade == "C"){
+  //   echo "You did okay";
+  // }
+  // else if($grade == "D"){
+  //   echo "You did poorly";
+  // }
+  // else if($grade == "F"){
+  //   echo "You failed";
+  // }
+  // else {
+  //   echo"{$grade} is not a valid grade";
+  // }
   
+ 
+$grade = "A"; 
+  
+switch($grade){
+  case "A":
+    echo"You did great";
+  break;
+
+  case "B":
+    echo"You did good";
+  break;
+
+  case "C":
+    echo"You did okay!";
+  break;
+
+  case "D":
+    echo"You did poorly";
+  break;
+
+  case "F":
+    echo"You failed";
+  break;
+  
+}
+
 
 
 ?>
