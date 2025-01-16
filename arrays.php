15cda85154d41b6b97421494c333174bab4ceb6c
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-16 08:58:37 -0300

    Update arrays.php

---
+++ arrays.php
@@ -0,0 +1,23 @@
+<?php
+//array = "variable" which can hold more than one value at a time
+
+$food_1 = "apple";
+$food_2 = "orange";
+$food_3 = "banana";
+$food_4 = "coconut";
+
+$foods = array("apple","orange","banana","coconut");
+
+// $foods[0] = "pineapple";
+// array_push($foods, "pineapple","kiwi");
+// array_pop($foods); //removes the last array element
+// array_shift($foods); //removes the first array element
+// $reversed_foods = array_reverse($foods); //reverses an array
+// echo count($foods);
+
+
+
+
+// To fully display the elements of an array, use foreach() loop;
+
+?>
