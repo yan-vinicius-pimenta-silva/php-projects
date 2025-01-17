06dbbf91cc6c48d332111dac2bc557bf8929744b
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-17 13:09:17 -0300

    Update functions.php

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php 
+
+  // function = write some code once, reuse when you need it
+  //            type () after function name to invoke
+  //            ex. add() subtract() multiply() divide()
+
+
+ $name = null;
+ $age = null;
+
+  function happy_birthday($name,$age){
+    echo"Happy Birthday dear {$name}!<br>";
+    echo"Happy Birthday to you!<br>";
+    echo"Happy Birthday dear you!<br>";
+    echo"You are {$age} years old.<br> <br>";
+
+    return  
+    
+  }
+
+  happy_birthday("Yan",23);
+  happy_birthday("Yan",23);
+  happy_birthday("Yan",23);
+
+ 
