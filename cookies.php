0fc8bde97b4231de3fe910c536364ddb6d5f8d62
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-20 13:55:25 -0300

    update

---
+++ cookies.php
@@ -0,0 +1,25 @@
+<?php 
+
+  // cookie = Information about a user stored in a user's web-browser
+  //          targeted advertisements, browsing preferences, and
+  //          other non-sensitive data
+
+
+
+  setcookie("fav_food", "pizza", time() - (86400 * 2), "/" );
+  setcookie("fav_drink", "coffee", time() + (86400 * 3),"/" );
+  setcookie("fav_dessert", "ice cream", time() + (86400 * 4),"/" );
+  
+  foreach($_COOKIE as $key  => $value ) {
+    echo"{$key} = {$value} <br>";
+    
+
+
+  }
+  
+  if(isset($_COOKIE[""])) {
+    echo""
+  }
+    
+
+?>
