f23c44686c590319e4f9f23a3b71ee6b27262771
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-20 12:31:32 -0300

    this is my website

---
+++ locations.php
@@ -0,0 +1,23 @@
+<?php 
+  include("header.html");
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  This is the Locations page<br>
+  Stuff about your Locations page can go here<br>
+</body>
+
+</html>
+<?php 
+  include("footer.html");
+?>
