1214fb886b2ea72c2fc63aa91f9a818ea0e9ada5
yan-vinicius-pimenta-silva <yan.fedora@gmail.com>
2025-01-24 12:53:38 -0300

    final steps of the course

--- phpmyadmin-create-a-table/index.php
+++ phpmyadmin-create-a-table/index.php
@@ -1,13 +1,24 @@
-<!-- 
-  
-next task: php insert into mysql database
-
-
-
--->
-
 <?php 
 
-  include("database.php");
+  include("database.php")
+
+  $sql = "SELECT * FROM users WHERE user = 'Sandy'";
+  $result = mysqli_query($conn, $sql);
+
+  if(mysqli_num_rows($result) > 0){
+    while($row = mysqli_fetch_assoc($result));
+    echo $row["id"] . "<br>"; 
+    echo $row["user"] . "<br>";
+    echo $row ["reg_date"] . "<br>";
+    
+    
+  }
+  else{
+    echo"No user found";
+    
+
+  }
+  
+  mysqli_close($conn);
 
 ?>
